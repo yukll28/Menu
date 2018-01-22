8bfe1916a99ba2416e44f3ef6d8d58ba22c038fa
50616021 <50616021@50616021-pc>
2018-01-22 09:11:25 +0900

    50616021

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view frame = new Menu_view();
+		frame.setBounds(5,5,755,565);
+		frame.setVisible(true);
+		
+	}
+
+}
